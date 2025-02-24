c01d47d4d634494ba06bee46e801fdff71488e64
saabarindirie <sxd247@student.bham.ac.uk>
2025-02-24 15:07:24 +0000

    add background color to review page

--- src/main/webapp/app/entities/review/list/review.component.ts
+++ src/main/webapp/app/entities/review/list/review.component.ts
@@ -17,6 +17,7 @@ import { ReviewDeleteDialogComponent } from '../delete/review-delete-dialog.comp
   standalone: true,
   selector: 'jhi-review',
   templateUrl: './review.component.html',
+  styleUrl: './review.component.scss',
   imports: [
     RouterModule,
     FormsModule,
