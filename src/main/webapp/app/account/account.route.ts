5100e891869c6ae2a48a6889cab6b164e4328b0e
Naylea <SNS381@student.bham.ac.uk>
2025-03-13 14:31:11 +0000

    edited item listing page

--- src/main/webapp/app/account/account.route.ts
+++ src/main/webapp/app/account/account.route.ts
@@ -6,6 +6,8 @@ import passwordResetFinishRoute from './password-reset/finish/password-reset-fin
 import passwordResetInitRoute from './password-reset/init/password-reset-init.route';
 import registerRoute from './register/register.route';
 import settingsRoute from './settings/settings.route';
+import { Authority } from '../config/authority.constants';
+import { UserRouteAccessService } from '../core/auth/user-route-access.service';
 
 const accountRoutes: Routes = [
   activateRoute,
@@ -14,6 +16,19 @@ const accountRoutes: Routes = [
   passwordResetInitRoute,
   registerRoute,
   settingsRoute,
+
+  // {
+  //   path: 'profile-details',
+  //   component: ProfileDetailComponent,
+  //   canActivate: [UserRouteAccessService],
+  //   data: { authorities: [Authority.ADMIN] }, // Admin access to any profile
+  // },
+  // {
+  //   path: 'profile-details/:id',
+  //   component: ProfileDetailComponent,
+  //   canActivate: [UserRouteAccessService],
+  //   data: { authorities: [Authority.USER, Authority.ADMIN] }, // Regular users can only access their own profile
+  // },
 ];
 
 export default accountRoutes;
