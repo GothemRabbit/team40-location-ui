19bed64ac8e9ddbbc5a3036b3a006d54a6972774
saabarindirie <sxd247@student.bham.ac.uk>
2025-03-17 20:44:16 +0000

    removing delete button from detail page and adding it to main page

--- src/main/webapp/app/entities/review/detail/review-detail.component.ts
+++ src/main/webapp/app/entities/review/detail/review-detail.component.ts
@@ -1,31 +1,22 @@
 import { Component, inject, input } from '@angular/core';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
 import { DurationPipe, FormatMediumDatePipe, FormatMediumDatetimePipe } from 'app/shared/date';
 import { DataUtils } from 'app/core/util/data-util.service';
 import { IReview } from '../review.model';
-import { EntityArrayResponseType, ReviewService } from '../service/review.service';
-import { ReviewDeleteDialogComponent } from '../delete/review-delete-dialog.component';
-import { filter, tap } from 'rxjs';
-import { ITEM_DELETED_EVENT } from '../../../config/navigation.constants';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   standalone: true,
   selector: 'jhi-review-detail',
   templateUrl: './review-detail.component.html',
-  styleUrl: './review-detail.component.scss',
-  imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
+  styleUrl: 'review-detail.component.scss',
+  imports: [SharedModule, RouterModule, FormatMediumDatePipe],
 })
 export class ReviewDetailComponent {
   review = input<IReview | null>(null);
 
   protected dataUtils = inject(DataUtils);
-  protected readonly reviewService = inject(ReviewService);
-  protected readonly activatedRoute = inject(ActivatedRoute);
-  protected modalService = inject(NgbModal);
-  protected readonly router = inject(Router);
 
   byteSize(base64String: string): string {
     return this.dataUtils.byteSize(base64String);
@@ -38,8 +29,8 @@ export class ReviewDetailComponent {
   previousState(): void {
     window.history.back();
   }
-
-  delete(review: IReview): void {
+}
+/*  delete(review: IReview): void {
     const modalRef = this.modalService.open(ReviewDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
     modalRef.componentInstance.review = review;
     // unsubscribe not needed because closed completes on modal close
@@ -51,5 +42,4 @@ export class ReviewDetailComponent {
         }),
       )
       .subscribe();
-  }
-}
+  } */
